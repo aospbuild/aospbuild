b5c025f78a84dbb41bae8134b2db215eb30c1c93
Your Name <you@example.com>
2018-06-18 07:15:51 -0800

    FDroid

--- script/clean_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rfv out/release-marlin*
-rm -rfv out/target/product/marlin/obj/PACKAGING/
-rm -rfv out/target/product/marlin/{boot.img,root,kernel,ramdisk*,recovery*,system*,vendor*}
-rm -rfv /tmp/{tmp*,system*,vendor*,target*}
